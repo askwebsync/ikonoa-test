a77cd09e7b4930b0af82c949590ba02128eb09f6
ask.websync <ask.websync@gmail.com>
2023-04-11 20:12:47 +0700

    homepage revisxing

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="border-b-2">
     <nav
-      class="container p-4 mx-auto md:flex md:justify-between md:items-center xl:px-6"
+      class="container px-4 md:px-8 lg:px-24 mx-auto md:flex md:justify-between md:items-center"
     >
       <div class="flex items-center justify-between focus:outline-none">
         <router-link :to="{ name: 'home' }">
