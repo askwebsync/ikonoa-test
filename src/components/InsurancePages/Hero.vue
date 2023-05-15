dac3fe4c035d47537c60f61b75d16ab96f93d1b8
ask.websync <ask.websync@gmail.com>
2023-05-15 09:16:52 +0700

    rename background lp insurance hero

--- src/components/InsurancePages/Hero.vue
+++ src/components/InsurancePages/Hero.vue
@@ -4,7 +4,7 @@
       class="relative overflow-hidden bg-no-repeat bg-cover"
       style="
         background-position: 50%;
-        background-image: url('/assets/img/LP_Insurance/background.png');
+        background-image: url('/assets/img/LP_Insurance/herobackground.png');
         height: 460px;
       "
     >
