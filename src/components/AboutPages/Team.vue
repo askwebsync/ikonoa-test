10dc541a2b6c15a1e19fbd3f00bbbdd8555ecc7d
ask.websync <ask.websync@gmail.com>
2023-05-17 17:08:39 +0700

    fixing revisi our team

--- src/components/AboutPages/Team.vue
+++ src/components/AboutPages/Team.vue
@@ -2,28 +2,43 @@
   <section class="container mx-auto px-4 py-12 md:px-8 lg:px-24">
     <div class="flex flex-col items-center justify-center gap-10 space-y-8">
       <div class="flex flex-col gap-2 text-center">
-        <h3 class="text-xl md:text-2xl lg:text-3xl">MEET THE TEAM</h3>
+        <h3 class="text-xl md:text-2xl lg:text-3xl">OUR TEAM</h3>
         <h2 class="text-lg md:text-xl lg:text-2xl text-gray-300">
           Some of the people you'll be working with
         </h2>
       </div>
-      <div class="flex flex-wrap justify-center gap-4">
+      <div class="flex flex-wrap justify-center">
+        <!-- Member -->
         <div
           v-for="(slide, index) in slides"
           :key="index"
-          class="flex flex-col justify-center mx-2 text-center w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6"
+          class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-6 px-6 sm:px-6 lg:px-4"
         >
-          <div
-            class="flex items-center justify-center self-center w-24 h-24 mb-2 rounded-full bg-center bg-cover dark:bg-gray-500"
-          >
-            <img
-              alt="image"
-              class="w-full h-full rounded-full"
-              :src="slide.image"
-            />
+          <div class="flex flex-col gap-5">
+            <!-- Avatar -->
+
+            <div class="aspect-w-1 aspect-h-1 self-center">
+              <img
+                class="object-fit rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
+                :src="slide.image"
+                alt="Avatar"
+                style="width: 160px; height: 180px"
+              />
+            </div>
+
+            <!-- Details -->
+            <div class="text-center mt-6">
+              <!-- Name -->
+              <h1 class="text-gray-900 text-xl font-bold mb-1">
+                {{ slide.name }}
+              </h1>
+
+              <!-- Title -->
+              <div class="text-gray-700 font-light mb-2">
+                {{ slide.title }}
+              </div>
+            </div>
           </div>
-          <p class="mt-1 text-base font-semibold">{{ slide.name }}</p>
-          <p class="mt-1 text-gray-500 dark:text-gray-400">{{ slide.title }}</p>
         </div>
       </div>
     </div>
