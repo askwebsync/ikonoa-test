be58d96b656a47423db726e10f37afe54c95d50d
ask.websync <ask.websync@gmail.com>
2023-05-19 08:48:30 +0700

    rapihin meta tag index.html + css di app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div v-if="loading" class="h-screen flex items-center justify-center">
+  <!-- <div v-if="loading" class="h-screen flex items-center justify-center">
     <div class="spinner w-12 h-12 mx-auto text-gray-600 animate-spin"></div>
-  </div>
-  <div class="app-container" v-if="!loading">
+  </div> -->
+  <div class="app-container">
     <app-header />
     <router-view v-slot="{ Component, route }">
       <transition name="fade" mode="out-in">
@@ -35,86 +35,17 @@ export default {
   },
   data() {
     return {
-      loading: true,
+      // loading: true,
     };
   },
-  created() {
-    setTimeout(() => {
-      if (this.loading) {
-        this.loading = false;
-      }
-    }, 1000);
-  },
+  // created() {
+  //   setTimeout(() => {
+  //     if (this.loading) {
+  //       this.loading = false;
+  //     }
+  //   }, 1000);
+  // },
 };
 </script>
 
-<style>
-.app-container {
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-}
-
-.router-view {
-  flex: 1;
-}
-
-.app-footer {
-  margin-top: auto;
-}
-.slideFade-enter-active,
-.slideFade-leave-active {
-  transition: opacity 1s;
-}
-
-.slideFade-enter,
-.slideFade-leave-to {
-  opacity: 0;
-}
-
-.spinner {
-  border: 5px solid rgba(0, 0, 0, 0.1);
-  border-left-color: #3490dc;
-  border-radius: 50%;
-  animation: spin 1s linear infinite;
-}
-
-@keyframes spin {
-  to {
-    transform: rotate(360deg);
-  }
-}
-
-.float {
-  position: fixed;
-  width: 60px;
-  height: 60px;
-  bottom: 25px;
-  right: 25px;
-  background-color: #25d366;
-  color: #fff;
-  border-radius: 50px;
-  text-align: center;
-  font-size: 30px;
-  box-shadow: 2px 2px 3px #999;
-  z-index: 100;
-}
-
-.my-float {
-  margin-top: 16px;
-}
-
-@media (max-width: 768px) {
-  .float {
-    width: 50px;
-    height: 50px;
-    bottom: 15px;
-    right: 15px;
-    font-size: 24px;
-  }
-
-  .my-float {
-    margin-top: 12px;
-  }
-}
-</style>
+<style></style>
