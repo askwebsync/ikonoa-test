d096431ebf0a70585467039f75e9e9bb8ef2eb65
ask.websync <ask.websync@gmail.com>
2023-05-19 11:09:56 +0700

    fixing avoid layouts of hero section

--- src/components/Homepages/Hero.vue
+++ src/components/Homepages/Hero.vue
@@ -12,6 +12,7 @@
         class="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
         style="background-color: rgba(0, 0, 0, 0.65)"
       >
+        <div class="aspect-w-16 aspect-h-9"></div>
         <div
           class="flex flex-col justify-center items-center h-full text-center gap-6 container mx-auto px-4 md:px-8 lg:px-24"
         >
