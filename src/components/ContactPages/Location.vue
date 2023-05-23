aea0a101a124155b8b71c7625a76904626a247d6
ask.websync <ask.websync@gmail.com>
2023-05-23 16:43:17 +0700

    fixing revisi

--- src/components/ContactPages/Location.vue
+++ src/components/ContactPages/Location.vue
@@ -5,7 +5,7 @@
         <h3 class="text-xl md:text-2xl lg:text-3xl">LOCATION</h3>
         <h2 class="text-xl md:text-2xl lg:text-3xl">Find Us on Google Map</h2>
       </div>
-      <div class="google-map my-12 rounded">
+      <div class="google-map m-12 rounded">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7931.962574497798!2d106.82478594107462!3d-6.266191024889744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f38e575183bd%3A0xbffac3daf2cfe981!2sGraha%20Al%20Badegel!5e0!3m2!1sen!2sid!4v1684310944531!5m2!1sen!2sid"
           width="100%"
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,146 +1,135 @@
 <template>
   <div class="container mx-auto px-4 py-12 md:px-8 lg:px-24">
     <div
-      class="gap-y-16 flex flex-col justify-between text-center lg:text-left md:gap-x-6 lg:flex-row lg:gap-y-0"
+      class="grid grid-cols-1 gap-12 md:gap-16 lg:gap-24 text-center lg:grid-cols-4 lg:text-left"
     >
-      <div class="lg:w-1/3">
-        <div class="flex flex-col items-center lg:items-start gap-2">
-          <img
-            src="/assets/img/Icon/logo.png"
-            class="w-28 h-12 object-contain"
-            alt="logo"
-            width="112"
-            height="48"
-          />
+      <div class="flex flex-col items-center lg:items-start gap-6">
+        <img
+          src="/assets/img/Icon/logo.png"
+          class="w-28 h-12 object-contain"
+          alt="logo"
+          width="112"
+          height="48"
+        />
 
-          <p class="text-md md:text-lg font-medium max-w-xs text-white">
-            {{ $t("footer.card1-header") }}
-          </p>
-        </div>
+        <p class="text-md md:text-lg font-medium max-w-xs text-white">
+          {{ $t("footer.card1-header") }}
+        </p>
       </div>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:w-2/3 lg:ml-3 gap-8">
-        <div class="space-y-4">
-          <h3 class="text-lg">{{ $t("footer.card2-header") }}</h3>
-          <ul class="space-y-2 text-white">
-            <li class="mb-2">
-              <router-link
-                :to="{ name: 'about' }"
-                class="text-md md:text-lg font-medium block w-full"
-              >
-                {{ $t("footer.card2-option1") }}
-              </router-link>
-            </li>
-            <li class="mb-2">
-              <router-link
-                :to="{ name: 'services' }"
-                class="text-md md:text-lg font-medium block w-full"
-              >
-                {{ $t("footer.card2-option2") }}
-              </router-link>
-            </li>
-            <li class="mb-2">
-              <router-link
-                :to="{ name: 'insurance-policy' }"
-                class="text-md md:text-lg font-medium block w-full"
-              >
-                {{ $t("footer.card2-option3") }}
-              </router-link>
-            </li>
-            <li class="mb-2">
-              <router-link
-                :to="{ name: 'claim-experience' }"
-                class="text-md md:text-lg font-medium block w-full"
-              >
-                {{ $t("footer.card2-option4") }}
-              </router-link>
-            </li>
-            <li class="">
-              <router-link
-                :to="{ name: 'contact' }"
-                class="text-md md:text-lg font-medium block w-full"
-              >
-                {{ $t("footer.card2-option5") }}
-              </router-link>
-            </li>
-          </ul>
-        </div>
-        <div class="space-y-4">
-          <h3 class="text-lg">{{ $t("footer.card3-header") }}</h3>
-          <ul class="space-y-2 text-white">
-            <li class="mb-2">
-              <router-link
-                :to="{ name: 'risk-consulting' }"
-                class="text-md md:text-lg font-medium block w-full"
-              >
-                {{ $t("footer.card3-option1") }}
-              </router-link>
-            </li>
-            <li class="mb-2">
-              <router-link
-                :to="{ name: 'risk-management' }"
-                class="text-md md:text-lg font-medium block w-full"
-              >
-                {{ $t("footer.card3-option2") }}
-              </router-link>
-            </li>
-            <li class="mb-2">
-              <router-link
-                :to="{ name: 'insurance-policy' }"
-                class="text-md md:text-lg font-medium block w-full"
-              >
-                {{ $t("footer.card3-option3") }}
-              </router-link>
-            </li>
-            <li class="mb-2">
-              <router-link
-                :to="{ name: 'academy' }"
-                class="text-md md:text-lg font-medium block w-full"
-              >
-                {{ $t("footer.card3-option4") }}
-              </router-link>
-            </li>
-          </ul>
-        </div>
-        <div class="space-y-4">
-          <h3 class="text-lg">{{ $t("footer.card4-header") }}</h3>
-          <ul class="space-y-3 text-white">
-            <ul class="space-y-3 text-white">
-              <li class="mb-2">
-                <a class="text-md md:text-lg font-medium block w-full" href="#"
-                  >(+62) 811-963754</a
-                >
-              </li>
-              <li class="mb-2">
-                <a class="text-md md:text-lg font-medium block w-full" href="#"
-                  >hallo@ikonoa.com</a
-                >
-              </li>
-              <li class="mb-2 hidden md:block">
-                <p class="text-md md:text-lg font-medium">
-                  {{ $t("footer.card4-option1") }}
-                  <span class="italic text-gray-300"
-                    >Graha Mampang 3rd Floor, Suite 305, Jl. Mampang Prapatan
-                    Raya Kav. 100, Jakarta 12760</span
-                  >
-                </p>
-              </li>
-              <li class="mb-2 hidden md:block">
-                <p class="text-md md:text-lg font-medium italic">
-                  {{ $t("footer.card4-option2") }}
-                  <span class="italic text-gray-300"
-                    >Graha Al Badegel, 2nd Floor, Jl. Tutty Alawiyah No. 7
-                    Jakarta Selatan – 12740</span
-                  >
-                </p>
-              </li>
-            </ul>
-          </ul>
-        </div>
+      <div>
+        <h3 class="text-lg mb-4">{{ $t("footer.card2-header") }}</h3>
+        <ul class="space-y-3 text-white">
+          <li class="">
+            <router-link
+              :to="{ name: 'about' }"
+              class="text-md md:text-lg font-medium block w-full"
+            >
+              {{ $t("footer.card2-option1") }}
+            </router-link>
+          </li>
+          <li class="">
+            <router-link
+              :to="{ name: 'services' }"
+              class="text-md md:text-lg font-medium block w-full"
+            >
+              {{ $t("footer.card2-option2") }}
+            </router-link>
+          </li>
+          <li class="">
+            <router-link
+              :to="{ name: 'insurance-policy' }"
+              class="text-md md:text-lg font-medium block w-full"
+            >
+              {{ $t("footer.card2-option3") }}
+            </router-link>
+          </li>
+          <li class="">
+            <router-link
+              :to="{ name: 'claim-experience' }"
+              class="text-md md:text-lg font-medium block w-full"
+            >
+              {{ $t("footer.card2-option4") }}
+            </router-link>
+          </li>
+          <li class="">
+            <router-link
+              :to="{ name: 'contact' }"
+              class="text-md md:text-lg font-medium block w-full"
+            >
+              {{ $t("footer.card2-option5") }}
+            </router-link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3 class="text-lg mb-4">{{ $t("footer.card3-header") }}</h3>
+        <ul class="space-y-3 text-white">
+          <li class="">
+            <router-link
+              :to="{ name: 'risk-consulting' }"
+              class="text-md md:text-lg font-medium block w-full"
+            >
+              {{ $t("footer.card3-option1") }}
+            </router-link>
+          </li>
+          <li class="">
+            <router-link
+              :to="{ name: 'risk-management' }"
+              class="text-md md:text-lg font-medium block w-full"
+            >
+              {{ $t("footer.card3-option2") }}
+            </router-link>
+          </li>
+          <li class="">
+            <router-link
+              :to="{ name: 'insurance-policy' }"
+              class="text-md md:text-lg font-medium block w-full"
+            >
+              {{ $t("footer.card3-option3") }}
+            </router-link>
+          </li>
+          <li class="">
+            <router-link
+              :to="{ name: 'academy' }"
+              class="text-md md:text-lg font-medium block w-full"
+            >
+              {{ $t("footer.card3-option4") }}
+            </router-link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3 class="text-lg mb-4">{{ $t("footer.card4-header") }}</h3>
+        <ul class="space-y-3 text-white">
+          <li class="">
+            <a class="text-md md:text-lg font-medium block w-full" href="#"
+              >(+62) 811-963754</a
+            >
+          </li>
+          <li class="">
+            <a class="text-md md:text-lg font-medium block w-full" href="#"
+              >hallo@ikonoa.com</a
+            >
+          </li>
+          <li class="">
+            <p class="text-md md:text-lg font-medium block w-full">
+              {{ $t("footer.card4-option1") }}
+            </p>
+          </li>
+          <li class="">
+            <p class="text-md md:text-lg font-medium block w-full">
+              {{ $t("footer.card4-option2") }}
+            </p>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
-  <div class="py-8 text-sm text-center text-white border-t">
-    © 2023 PT.Ikonoa Solusindo Asia. All rights reserved.
+
+  <div class="mt-12 border-t border-gray-100 py-4">
+    <p class="text-sm text-center text-white">
+      © 2023 PT.Ikonoa Solusindo Asia. All rights reserved.
+    </p>
   </div>
 </template>
 
@@ -161,16 +150,12 @@ li p {
 
 h3 {
   color: #ffff00;
+  letter-spacing: -0.25px;
 }
 
 footer a {
-  padding: 8px;
   display: inline-block;
   width: 100%;
   box-sizing: border-box;
-}
-
-footer a:hover {
-  background-color: #333;
 }
 </style>
